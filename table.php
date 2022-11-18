7369a6e1f39cf4e3f458642f6075f6ac39b6c9c9
Harshith2502 <111785160+Harshith2502@users.noreply.github.com>
2022-11-18 22:25:34 +0530

    Add files via upload

---
+++ table.php
@@ -0,0 +1,30 @@
+<?php
+include_once('about.php');
+$query="select * from signup";
+$result=mysql_query($query);
+?>
+
+<!DOCTYPE html>
+<html>
+    <title>
+        <head>
+            Fetch Data From Database</head>
+</title>
+<body>
+    <table align="center" border="lpx" style="width :300px; line-height:30px;">
+    <tr>
+        <th colspan="4"><h2>Student Record</h2></th>
+</tr>
+<t>
+    <th> ID</th>
+    <th> ID</th>
+    <th> ID</th>
+    <th> ID</th>
+    <th> ID</th>
+    <th> ID</th>
+    <th> ID</th>
+</t>
+</table>
+
+</body>
+</html>
